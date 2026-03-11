e055d96e502badffd957757f8494514410be37c1
mtai0524 <minhnguyen20020524@gmail.com>
2026-03-11 15:40:19 +0700

    feat: Initialize Svelte desktop frontend project with dependencies and build configuration.

--- src/go.go
+++ src/go.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello, World!")
+	fmt.Print("Hello, World!\n")
+	fmt.Print(sum(1, 2))
+	fmt.Print("\n")
+	fmt.Print(sumSlice([]int{1, 2, 3, 4, 5}))
+}
+
+// Hàm tính tổng 2 số nguyên
+func sum(a, b int) int {
+	return a + b
+}
+
+// Hàm tính tổng một mảng (slice) các số nguyên
+func sumSlice(numbers []int) int {
+	total := 0
+	for _, num := range numbers {
+		total += num
+	}
+	return total
 }
